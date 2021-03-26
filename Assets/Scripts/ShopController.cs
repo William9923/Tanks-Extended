3d8bc7409a51d763a4b3859ef67073e96483501c
fabianusharry <fabianusharry@yahoo.com>
2021-03-26 14:10:09 +0700

    shop tinggal constraint

--- Assets/Scripts/ShopController.cs
+++ Assets/Scripts/ShopController.cs
@@ -11,6 +11,7 @@ public class ShopController : MonoBehaviour
     private GameObject myEventSystem;
     private bool isOpened;
     [HideInInspector] public GameObject m_Instance;
+    private TankMoney money;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class ShopController : MonoBehaviour
         isOpened = false;
         m_ShopMenu.SetActive(isOpened);
         myEventSystem = GameObject.Find("EventSystem");
+        
     }
 
     void Setup() 
@@ -34,6 +36,14 @@ public class ShopController : MonoBehaviour
         buyChar2Btn.onClick.AddListener(buyChar2);
     }
 
+    void DestroySetup()
+    {
+        upgradeWeaponBtn.onClick.RemoveListener(upgradeWeapon);
+        buyShellsBtn.onClick.RemoveListener(buyShells);
+        buyChar1Btn.onClick.RemoveListener(buyChar1);
+        buyChar2Btn.onClick.RemoveListener(buyChar2);
+    }
+
     public void Toggle(GameObject tank)
     {
         m_ShopMenu.SetActive(!isOpened);
@@ -43,39 +53,52 @@ public class ShopController : MonoBehaviour
             Setup();
             Debug.Log(tank);
             m_Instance = tank;
-            TankMoney money = m_Instance.GetComponent<TankMoney>();
+            money = m_Instance.GetComponent<TankMoney>();
             moneyAmount.text = money.GetMoney().ToString();
-        }    
+        }
+        if (!isOpened)
+        {
+            DestroySetup();
+        }
     }
 
     
 
     void upgradeWeapon()
     {
+        money.BuyItem(3000);
         Debug.Log("Upgrade gan");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null); //Menghapus state selected
     }
 
     void buyShells()
     {
+        money.BuyItem(100);
         Debug.Log("Buy Shells");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
     }
 
     void buyChar1()
     {
+        money.BuyItem(1000);
         Debug.Log("Buy Character 1");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
     }
 
     void buyChar2()
     {
+        money.BuyItem(1500);
         Debug.Log("Buy Character 2");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
     }
     // Update is called once per frame
     void Update()
     {
+        if (isOpened)
+        {
+            money = m_Instance.GetComponent<TankMoney>();
+            moneyAmount.text = money.GetMoney().ToString();
+        }
         
     }
 }
